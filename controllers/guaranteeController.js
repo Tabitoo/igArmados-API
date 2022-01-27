83a0e4378c9fcd67a22f3e52303e2cf90a1762e3
Tabitoo <lautarovaldez4@gmail.com>
2022-01-27 17:57:20 -0300

    guarantee controller changed

--- controllers/guaranteeController.js
+++ controllers/guaranteeController.js
@@ -1,33 +1,48 @@
-const db = require("../database/models");
- module.exports = {
-     getGuarantees : (req,res) => {
-         db.Guarantees.findAll()
-         .then(result => {
-             res.status(200).json({
-                 meta : {
-                     status : 200,
-                     amount : result.length 
-                 },
-                 data : result
-             })
-         }).catch(error => console.log(error))
-     },
-     getOneGuarantee : (req,res) => {
-         db.Guarantees.findByPk(req.params.id)
-         .then(result => {
-             if(result){
-                 res.status(200).json({
-                     meta : {
-                         status : 200
-                     },
-                     data : result
-                 })
-             }else {
-                 res.status(404).json({
-                     status : 404,
-                     msg : 'Component not found'
-                 })
-             }
-         }).catch(error => console.log(error))
-     }
- }
+const guaranteesService = require('../services/guarantees');
+
+const getAll = async (req, res, next) => {
+    try {
+        const data = await guarantees.getAll();
+        res.status(200).json({ data : data })
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+const getById = async (req, res, next) => {
+    try {
+        const data = await guaranteesService.getById(req.params.id);
+        res.status(200).json({ data : data })
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+const create = async(res, req, next) => {
+    try {
+        const data = await guaranteesService.create(req.body)
+        res.status(201).json({ msg : 'guarantee created succesfully', data })
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+const update = async(res, req, next) => {
+    try {
+        const data = await guaranteesService.update(req.params.id, req.body);
+        res.status(200).json({ msg : 'guarantee updated succesfully', data })
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update
+}
