dc6ce44dbe0974ae7a96e26846906293fad6a67b
Tabitoo <lautarovaldez4@gmail.com>
2022-01-27 15:42:31 -0300

    Create and update method added

--- repositories/categories.js
+++ repositories/categories.js
@@ -1,5 +1,10 @@
 const db = require('../database/models');
 
+const create = async (body) => {
+    return await db.Categorys.create(body)
+    
+}
+
 const getAll = async () => {
     return await db.Categorys.findAll()
 }
@@ -8,7 +13,15 @@ const getById = async (id) => {
     return await db.Categorys.findByPk(id)
 }
 
+const update = async (id, body) => {
+    return await db.Categorys.update(body, {
+        where : {id : id}
+    })
+}
+
 module.exports = {
+    create,
     getAll,
-    getById
+    getById,
+    update
 }
--- services/categories.js
+++ services/categories.js
@@ -1,14 +1,43 @@
+const createError = require('http-errors')
 const categoriesRepository = require('../repositories/categories')
+
+
+const create = async (body) => {
+    const data = categoriesRepository.create(body)
+
+    if(!data) { throw createError(400) }
+
+    return data
+
+}
+
+const update = async (id, body) => {
+    const category = categoriesRepository.getById(id)
+
+    if(!category) { throw createError(404, 'category not found') }
+
+    const updatedCategory =  await categoriesRepository.update(id, body)
+
+    if(updatedCategory[0] !== 1) { throw createError(400, "category could't be updated") }
+
+    return await categoriesRepository.getById(id)
+}
 
 const getAll = async () => {
     return await categoriesRepository.getAll()
 }
 
 const getById = async (id) => {
-    return await categoriesRepository.getById(id)
+    const category = categoriesRepository.getById(id)
+
+    if(!category) { throw createError(404, 'category not found') }
+    
+    return category
 } 
 
 module.exports = {
+    create,
     getAll,
-    getById
+    getById,
+    update
 }
