5d03a2dc894642a798909ac91d40cc4cf21b6c20
Tabitoo <lautarovaldez4@gmail.com>
2022-01-31 08:34:08 -0300

    Documentation with swagger added

---
+++ swaggerConfig/swaggerOptions.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'IG-Armados',
+          version: '0.0.1',
+          description:
+            'This is a CRUD API application made with Express and documented with Swagger for IG-Armados',
+        },
+        servers: [
+          {
+            url: `http://localhost:${process.env.PORT || 3002}`,
+            description: 'Development server',
+          },
+        ],
+      },
+      apis: ['./docs/*'],
+}
+
+module.exports = options;
