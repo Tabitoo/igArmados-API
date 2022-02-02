3db59a2c96221b302f7a0c0bbe219e89fc993680
Tabitoo <lautarovaldez4@gmail.com>
2022-02-02 15:47:10 -0300

    quick fixes and middleware added

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,14 @@
 var express = require('express');
 const productController = require('../controllers/productController')
+const upload = require('../middlewares/uploadImage')
 var router = express.Router();
 
 router.get('/search',productController.searchProduct);
 router.get('/',productController.getAll);
 router.get('/:id', productController.getById);
 router.get('/randomproducts/:limit', productController.getsRandomProducts);
-router.post('/', productController.create);
-router.put('/:id', peoductController.update);
-router.delete('/:id',removeProduct);
+router.post('/', upload.any(), productController.create);
+router.put('/:id', productController.update);
+router.delete('/:id',productController.remove);
 
 module.exports = router;
