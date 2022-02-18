24483a78408313b5118025d667dddf5bf9cc6782
Tabitoo <lautarovaldez4@gmail.com>
2022-02-18 20:17:41 -0300

    quick fixes

--- controllers/markController.js
+++ controllers/markController.js
@@ -1,11 +1,11 @@
-const componentsService = require('../services/components');
+const marksService = require('../services/marks');
 const paginationParams = require('../modules/paginationParams')
 
 
-const getAll = async(req,res,next) => {
+const getAll = async(req, res, next) => {
     try {
         const params = paginationParams.generate(req);
-        const data = await componentsService.getAll(params)
+        const data = await marksService.getAll(params)
         res.status(200).json({ data })
 
     } catch (err) {
@@ -15,7 +15,7 @@ const getAll = async(req,res,next) => {
 
 const getById = async (req, res, next) => {
     try {
-        const data = await componentsService.getById(req.params.id);
+        const data = await marksService.getById(req.params.id);
         res.status(200).json({ data : data })
 
     } catch (err) {
@@ -25,8 +25,8 @@ const getById = async (req, res, next) => {
 
 const create = async(req, res, next) => {
     try {
-        const data = await componentsService.create(req.body)
-        res.status(201).json({ msg : 'component created succesfully', data })
+        const data = await marksService.create(req.body)
+        res.status(201).json({ msg : 'category created succesfully', data })
 
     } catch (err) {
         next(err)
@@ -35,8 +35,8 @@ const create = async(req, res, next) => {
 
 const update = async(req, res, next) => {
     try {
-        const data = await componentsService.update(req.params.id, req.body);
-        res.status(200).json({ msg : 'component updated succesfully', data })
+        const data = await marksService.update(req.params.id, req.body);
+        res.status(200).json({ msg : 'mark updated succesfully', data })
     }
     catch (err) {
         next(err)
@@ -45,8 +45,8 @@ const update = async(req, res, next) => {
 
 const remove = async(req, res, next) => {
     try {
-        await componentsService.remove(req.params.id)
-        res.status(200).json({ msg: 'component removed succesfully'})
+        await marksService.remove(req.params.id)
+        res.status(200).json({ msg: 'mark removed succesfully'})
     } catch (err) {
         next(err)
         
